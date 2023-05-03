84275fe7284e4ad13b3268958439431adaf99aa0
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-05-03 16:07:50 -0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+//we need a variable to store the api url
+
+//get all of our id's and and set them to a variable
+let place = document.getElementById('place');
+let temp = document.getElementById('temp');
+let temp_min = document.getElementById('temp_min');
+let temp_max= document.getElementById('temp_max');
+let feels_like = document.getElementById('feels_like');
+let speed = document.getElementById('speed');
+let deg = document.getElementById('deg');
+let search = document.getElementById('search');
+let btn = document.getElementById('btn');
+let favBtn = document.getElementById('favBtn');
+let injectFav = document.getElementById('inject');
+let favArr = [];
+let weatherArr = [];
+let searchedCity = '';
+
+//fav button event listener
+favBtn.addEventListener('click', e => {
+    
+});
+
+//create fetch function to get weather data from openweather api fetchWeather
+
+
+//create function ot get weather data getWeather
+
